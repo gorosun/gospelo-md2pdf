dbf01ad43b369a25b6f2b106fa415bb65a8b2d2e
gorosun <goro-hayakawa@no-studio.net>
2026-01-13 23:29:44 +0900

    chore: bump version to 1.3.0

--- src/gospelo_md2pdf/__init__.py
+++ src/gospelo_md2pdf/__init__.py
@@ -7,7 +7,7 @@ This software uses WeasyPrint (BSD 3-Clause) and Python-Markdown (BSD 3-Clause).
 See LICENSE file for third-party license information.
 """
 
-__version__ = "1.1.2"
+__version__ = "1.3.0"
 
 from .converter import convert_md_to_pdf, convert_md_to_html, convert_html_to_pdf
 
